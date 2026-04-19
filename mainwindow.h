4637e6a8ad965c98c1a136988c10c930a5bbec9d
ArtKh29 <lemus29.12.07@gmail.com>
2026-04-19 22:45:16 +0200

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -6,22 +6,28 @@
 #include <QVector>
 #include "parent.h"
 
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget* parent = nullptr);
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
 
 private slots:
     void loadFile();
-    void onDoubleClick(const QModelIndex& index);
+    void onDoubleClick(const QModelIndex &index);
 
 private:
-    QTableWidget* tableWidget = nullptr;
-    QVector<Parent> characters;
-
     void parseLine(const QString& line);
+
+    Ui::MainWindow *ui;
+    QTableWidget* tableWidget;
+    QVector<Parent> characters;
 };
 
-#endif // MAINWINDOW_H
+#endif
